f8002cb5bcb5ce857a0f9cd31a6a08d994d88c96
Luzzitto Tupaz <36060968+Luzzitto@users.noreply.github.com>
2025-06-28 10:43:46 -0400

    feat: add calculator, calendar, popover, and slider components with necessary styles and functionality

---
+++ app/calculator/page.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Slider } from "@/components/ui/slider";
+import React, { useState } from "react";
+
+const Calculator = () => {
+  const [sliderValue, setSliderValue] = useState<number>(0.1);
+  const [targetAmount, setTargetAmount] = useState<number>(0);
+  const [monthlyIncome, setMonthlyIncome] = useState<number>(0);
+
+  // Calculate the monthly savings based on percentage
+  const monthlySavings = monthlyIncome * sliderValue;
+
+  // Calculate months needed to reach the target
+  const monthsNeeded =
+    monthlySavings > 0 && targetAmount > 0
+      ? Math.ceil(targetAmount / monthlySavings)
+      : 0;
+
+  // Calculate years and months if monthsNeeded > 12
+  const years = Math.floor(monthsNeeded / 12);
+  const months = monthsNeeded % 12;
+
+  return (
+    <div className="min-h-screen p-8 ">
+      <Card className="max-w-6xl mx-auto">
+        <CardHeader>
+          <CardTitle>Savings Calculator</CardTitle>
+          <CardDescription>
+            This calculator helps you determine if your savings goal is
+            achievable based on your inputs. Enter your details to find out
+            whether reaching your target is possible or not.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Card className="w-full lg:max-w-2xl">
+            <CardContent className="grid grid-cols-1 gap-4">
+              <Label className="flex flex-col items-start space-y-1">
+                <span>Target Amount</span>
+                <Input
+                  type="number"
+                  value={targetAmount}
+                  min={0}
+                  onChange={(e) => setTargetAmount(Number(e.target.value))}
+                />
+              </Label>
+              <Label className="flex flex-col items-start space-y-1">
+                <span>Monthly Income</span>
+                <Input
+                  type="number"
+                  value={monthlyIncome}
+                  min={0}
+                  onChange={(e) => setMonthlyIncome(Number(e.target.value))}
+                />
+              </Label>
+              <Label className="flex flex-col items-start space-y-1">
+                <span>
+                  Percentage of Income: {(sliderValue * 100).toFixed(0)}%
+                </span>
+                <Slider
+                  value={[sliderValue]}
+                  max={1}
+                  min={0}
+                  step={0.05}
+                  onValueChange={([val]) => setSliderValue(val)}
+                />
+              </Label>
+              <div className="border p-8">
+                <h1>
+                  {monthlySavings > 0 && targetAmount > 0 ? (
+                    <>
+                      <div className="grid grid-cols-2">
+                        <p>Months Needed: </p>
+                        <span>
+                          {monthsNeeded > 12
+                            ? `${years} year${years > 1 ? "s" : ""}${
+                                months > 0
+                                  ? ` and ${months} month${
+                                      months > 1 ? "s" : ""
+                                    }`
+                                  : ""
+                              }`
+                            : `${monthsNeeded} months`}
+                        </span>
+                        <p>Savings Amount: </p>
+                        <span>${(sliderValue * monthlyIncome).toFixed(2)}</span>
+                      </div>
+                    </>
+                  ) : (
+                    <>Enter values to calculate.</>
+                  )}
+                </h1>
+              </div>
+            </CardContent>
+          </Card>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Calculator;
